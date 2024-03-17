bd6902cd9218cfdc8486416146e4cce167355242
Rafael De Almeida <124459052+RafaelDevProjects@users.noreply.github.com>
2024-03-16 21:51:36 -0300

    adicionando migrations

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
+
+
 def index(request):
-    return render(request, 'galeria/index.html')
+    dados = {
+    1: {"nome": "Nebulososa de Carina", 
+        "legenda": "webbbtelecope.com"},
+
+    2: {"nome": "galaxis", 
+        "legenda": "nasa.org"}
+    }
+    return render(request, 'galeria/index.html', {"cards": dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
